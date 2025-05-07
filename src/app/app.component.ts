944bd0cc41945ec025d9c33e764e3e9f4ba6fc5e
sunsonni <sonniemnguyen@gmail.com>
2025-05-07 12:41:12 -0500

    dobby is finally free from exams

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-
+import { RouterOutlet, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent], 
+  standalone: true,
+  imports: [RouterOutlet, RouterModule], 
   template: `
     <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
-      </header>
-      <section class="content">
-        <app-home></app-home>
-      </section>
+      <nav>
+        <a routerLink="/">Home</a>
+        <a routerLink="/about">about</a>
+      </nav>
+      <router-outlet></router-outlet>
     </main>
   `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'homes';
 }
