670ab2a3c197b62305ddd7bec3ddc1f395dde093
sunsonni <sonniemnguyen@gmail.com>
2025-05-08 22:51:19 -0500

    figuring it out

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,22 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { DetailsComponent } from './details/details.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'about', component: AboutComponent }
+    { 
+        path: '', 
+        component: HomeComponent,
+        title:'Home page'
+    },
+    { 
+        path: 'about', 
+        component: AboutComponent,
+        title: 'About page'
+    }, 
+    {
+        path: 'details/:id',
+        component: DetailsComponent,
+        title: 'Home details'
+    }
 ];
